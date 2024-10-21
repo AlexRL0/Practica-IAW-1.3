afad05d3dd81ba4b12e7a594ad0db537ce79ab5f
AlexRL0 <alejandrorl630@gmail.com>
2024-10-21 12:23:55 +0000

    cambios hechos

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,3 +11,11 @@ rm -rf /tmp/iaw-practica-lamp
 
 #Clonamos el repositorio de la aplicación en /tmp
 git clone https://github.com/josejuansanchez/iaw-practica-lamp.git /tmp/iaw-practica-lamp
+
+#Movemos el código fuente de la aplicación a /var/www/html
+mv /tmp/iaw-practica-lamp/src/* /var/www/html
+
+#Configuramos el archivo config.php
+sed -i "s/database_name_here/$DB_NAME/" /var/www/html/config.php
+sed -i "s/username_here/$DB_USER/" /var/www/html/config.php
+sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/config.php
