34a8f5c87622d27e428803b37fc68af5cfe29811
AlexRL0 <alejandrorl630@gmail.com>
2024-10-21 12:25:46 +0000

    añadimos lineas nuevas

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,3 +19,12 @@ mv /tmp/iaw-practica-lamp/src/* /var/www/html
 sed -i "s/database_name_here/$DB_NAME/" /var/www/html/config.php
 sed -i "s/username_here/$DB_USER/" /var/www/html/config.php
 sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/config.php
+
+#Creamos una base de datos de ejemplo
+mysql -u root <<< "DROP DATABASE IF EXISTS $DB_NAME"
+mysql -u root <<< "CREATE DATABASE $DB_NAME"
+
+#Creamos un usuario para la base de datos de ejemplo
+mysql -u root <<< "DROP USER IF EXISTS '$DB_USER'@'%'"
+mysql -u root <<< "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%'"
