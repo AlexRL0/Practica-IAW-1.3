9a34ead6cdcf388e2316973f842bc99540e29fd0
AlexRL0 <alejandrorl630@gmail.com>
2024-10-21 12:10:45 +0000

    deploy cambiado

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,8 @@ source .env
 #Para mostrar los comandos que se van ejecutando
 set -ex
 
-#Clonamos el repositorio de la aplicación
-git clone https://github.com/josejuansanchez/iaw-practica-lamp.git
+#Eliminamos clonados previos de la aplicación
+rm -rf /tmp/iaw-practica-lamp
+
+#Clonamos el repositorio de la aplicación en /tmp
+git clone https://github.com/josejuansanchez/iaw-practica-lamp.git /tmp/iaw-practica-lamp
